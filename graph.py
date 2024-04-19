64ce296161548d185e07a5b5c5d0cf69a404eaa9
eli8378 <143106098+Eli80763@users.noreply.github.com>
2024-04-18 20:19:22 -0500

    created gitignore and added new csv's to work with

--- graph.py
+++ graph.py
@@ -5,10 +5,11 @@ import seaborn as sns
 plt.rcParams["figure.figsize"] = [7.00, 3.50]
 plt.rcParams["figure.autolayout"] = True
 columns = ["Make", "Model", "Year", "Price", "Mileage"]
-df = pd.read_csv("cars_outliers_removed.csv", usecols=columns)
+df = pd.read_csv("resources/cars_outliers_removed.csv", usecols=columns)
 
 sns.scatterplot(x="Mileage", y="Price", data=years(2005,make("Toyota", df)))
 # sns.boxplot(x="Price", y="Make", hue="Year", data=years(2005,make("Porsche", df)))
 
 # sns.boxplot(x="Price", y="Make", hue="Year", data=years(2005,make("Honda", df)))
 plt.show()
+
