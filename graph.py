a7d1b6ee58cffd87876f0cecca2be2a05772d088
eli8378 <143106098+Eli80763@users.noreply.github.com>
2024-04-18 20:35:34 -0500

    made changes to graph.py format

--- graph.py
+++ graph.py
@@ -1,15 +1,20 @@
 import pandas as pd
 from matplotlib import pyplot as plt
-from functions import make, years, price
 import seaborn as sns
+from functions import make, years
+
+
 plt.rcParams["figure.figsize"] = [7.00, 3.50]
 plt.rcParams["figure.autolayout"] = True
 columns = ["Make", "Model", "Year", "Price", "Mileage"]
-df = pd.read_csv("resources/cars_outliers_removed.csv", usecols=columns)
 
-sns.scatterplot(x="Mileage", y="Price", data=years(2005,make("Toyota", df)))
+cars_df = pd.read_csv("resources/cars_outliers_removed.csv", usecols=columns)
+vehicles_df = pd.read_csv("resources/vehicles.csv")
+car_details_df = pd.read_csv("resources/CAR DETAILS FROM CAR DEKHO.csv")
+
+sns.scatterplot(x="Mileage", y="Price", data=years(2005,make("Toyota", cars_df)))
 # sns.boxplot(x="Price", y="Make", hue="Year", data=years(2005,make("Porsche", df)))
-
 # sns.boxplot(x="Price", y="Make", hue="Year", data=years(2005,make("Honda", df)))
+
 plt.show()
 
